61606fe54f7c62fb89432e024bdb3bb696ddddfb
Benjamin West <1023995@student.sae.edu.au>
2021-10-19 14:29:57 +1100

    Unity project added to repo

---
+++ Turtle Feeder/Assets/Scripts/NavigateToPellet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigateToPellet : MonoBehaviour
+{
+    public GameObject water;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}

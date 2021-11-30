0599189579fb3537575c7c7ce8667028b1d8c763
Benjamin West <1023995@student.sae.edu.au>
2021-11-30 12:52:40 +1100

    Trying to push with better internet

---
+++ Assets/Scripts/ConfirmationCube.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfirmationCube : MonoBehaviour
+{
+    private int handsInside = 0;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (hand.gameObject.name == "RightControllerAnchor" || hand.gameObject.name == "LeftControllerAnchor")
+        {
+            this.SetActive(true);
+            handsInside++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (hand.gameObject.name == "RightControllerAnchor" || hand.gameObject.name == "LeftControllerAnchor")
+        {
+            handsInside--;
+            if (handsInside == 0)
+            {
+                this.SetActive(false);
+            }
+        }
+    }
+}
